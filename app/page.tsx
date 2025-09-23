6e1d1de70d68bbae4fc88ea530a527658f996676
Amna Aftab Kifayat <160374615+amnakifayat13@users.noreply.github.com>
2025-09-24 00:10:37 +0500

    add greet to Arisha

--- app/page.tsx
+++ app/page.tsx
@@ -106,6 +106,7 @@ export default function Home() {
 
       <h1>Hello World!</h1>
       <h2>Hello Amna</h2>
+      <h1>Hello Arishah</h1>
       <section
         className="relative h-screen w-full flex items-center justify-center bg-cover bg-center"
         style={{ backgroundImage: "url(/ag_bg.jpg)" }}
